2540ec43a2e00be73d0af448ff4c5941deafbf73
EmanuelFirmino <ddos.emanuel@gmail.com>
2023-01-25 07:34:57 -0300

    inicio integração

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 from flask import Flask, render_template, request
 from database import database
+from random import randint
 
 class PAA:
 
     def __init__(self):
 
+        self.questions = ['O seu Pokémon é do tipo Fogo?', 'O seu Pokémon é quadrúpede?', 'O seu pokémon pesa mais de 64kg?']
+        self.curr_question = 2
         self.app = Flask(__name__, template_folder='./pages/html', static_folder='./pages/static')
         self.routes()
 
     def routes(self):
 
-        @self.app.route('/')
+        @self.app.route('/', methods=['GET', 'POST'])
         def index():
-            return render_template('index.html', title="Home")
+
+            sprite = f'../static/assets/pikachu{randint(1, 5)}.png'
+
+            if request.method == 'POST':
+                self.curr_question = (self.curr_question + 1) % len(self.questions)
+                    
+            return render_template('index.html', title="Home", question=self.questions[self.curr_question], sprite=sprite)
 
     def start(self):
 
