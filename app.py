8cab836ddae1d1f640d8262e757e53473f89f521
4tila <loboatila@gmail.com>
2023-01-25 18:54:30 -0400

    adcionei a arvore de decisao e fiz algumas mudancas

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request
 from database import database
 from random import randint
+def binary_tree(answer: list, tree: dict):
+    return tree[''.join(['1' if e else '0' for e in answer])]
 
 class PAA:
 
     def __init__(self):
 
         self.questions = ['O seu Pokémon é do tipo Fogo?', 'O seu Pokémon é quadrúpede?', 'O seu pokémon pesa mais de 64kg?']
-        self.curr_question = 2
+        self.answers = [False]*len(self.questions)
+        self.tree = {'111':'pikachu'}
+        self.curr_question = 0
         self.app = Flask(__name__, template_folder='./pages/html', static_folder='./pages/static')
         self.routes()
 
@@ -17,12 +21,14 @@ class PAA:
         def index():
 
             sprite = f'../static/assets/pikachu{randint(1, 5)}.png'
-
             if request.method == 'POST':
-                self.curr_question = (self.curr_question + 1) % len(self.questions)
-                    
-            return render_template('index.html', title="Home", question=self.questions[self.curr_question], sprite=sprite)
-
+                self.answers[self.curr_question] = request.form.get('positive')!=None
+                self.curr_question+=1
+            if self.curr_question < len(self.questions):
+                return render_template('index.html', title="Home", question=self.questions[self.curr_question], sprite=sprite)
+            else:
+                self.curr_question = 0
+                return render_template('index.html', title="Home", question="O pokemon eh o %s"%(binary_tree(self.answers,self.tree)), sprite=sprite)
     def start(self):
 
         self.app.run(debug=True)
