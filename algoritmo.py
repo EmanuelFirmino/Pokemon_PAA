3c098d532728fbb969414c0174c8d88aa2ad0803
Bruno Barros Xavier <110307733+200015621@users.noreply.github.com>
2023-02-13 06:53:50 -0300

    Update algoritmo.py

--- algoritmo.py
+++ algoritmo.py
@@ -8,17 +8,35 @@ class ALGORITMO:
 
         achou = not (db.get(ans, False)==False)
         question = ''
-        if len(ans)==0:
-            question = 'Seu Pokemon é leve?(pesa menos que 300 kg)'
-        if len(ans)==1:
-            question = 'Seu Pokemon é pequeno?(menor que 10 metros)'
-        if len(ans)==2:
-            question = 'Seu Pokemon é do  tipo  água?'
-        if len(ans)==3:
-            question = 'Seu Pokemon é do  tipo  normal?'
-        if len(ans)==4:
-            question = 'Seu Pokemon tem uma defesa fraca?(menor que 70)'
-        if len(ans)==5:
-            if achou: question = 'Seu Pokemon eh o %s'%(db[ans])
-            else: question = 'Nao achei seu Pokemon'
+
+        match len(ans):
+            case 0:
+                question = 'Seu Pokemon é leve?(pesa menos que 30 kg)'
+            case 1:
+                question = 'Seu Pokemon é pequeno?(menor que 1 metro)'
+            case 2:
+                question = 'Seu Pokemon tem pouca vida? (menor que 66)'
+            case 3:
+                question = 'Seu Pokemon tem um ataque fraco? (menor que 75)'
+            case 4:
+                question = 'Seu Pokemon tem uma defesa fraca? (menor que 70)'
+            case 5:
+                question = 'Seu Pokemon solta raios?'
+            case 3:
+                question = 'Seu Pokemon parece uma planta?'
+            case 4:
+                question = 'Seu Pokemon é do tipo água?'
+            case 5:
+                question = 'Seu Pokemon solta fogo?'
+            case 6:
+                question = 'Seu Pokemon se parece com um inseto?'
+            case 7:
+                question = 'Seu Pokemon possui asas?'
+            case 8:
+                question = 'Seu Pokemon é do tipo normal?'
+            case 9:
+                question = 'Seu Pokemon é venenoso?'
+            case 10:
+                if achou: question = 'Seu Pokemon eh o %s'%(db[ans])
+                else: question = 'Nao achei seu Pokemon'
         return question, achou
